3bdf67ddc42898cabd43e35a1d9de8a435f055fb
Cezary Bubieniec <pokion@outlook.com>
2024-03-25 14:08:54 +0100

    fix bug with scroll #86940dg3g

--- components/scripts/colorsPicker_testPanel.gd
+++ components/scripts/colorsPicker_testPanel.gd
@@ -3,5 +3,5 @@ extends Panel
 signal colorPicked(color)
 
 func _on_gui_input(event):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
 		colorPicked.emit(self.get_theme_stylebox("panel").bg_color)
